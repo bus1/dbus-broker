0fa2628124595fa3ec28f7e2a0405b46ec76d922
Tom Gundersen <teg@jklm.no>
2017-03-21 21:05:07 +0100

    peer: add basic test
    Signed-off-by: Tom Gundersen <teg@jklm.no>

---
+++ src/test-peer.c
@@ -0,0 +1,30 @@
+/*
+ * Test Peer
+ */
+
+#include <c-macro.h>
+#include <stdlib.h>
+#include "peer.h"
+#include "user.h"
+
+static void test_setup(void) {
+        UserRegistry *registry;
+        UserEntry *user;
+        Peer *peer;
+        int r;
+
+        assert(user_registry_new(&registry, 1024, 1024) >= 0);
+        assert(user_entry_ref_by_uid(registry, &user, 1) >= 0);
+
+        r = peer_new(&peer, user);
+        assert(r >= 0);
+
+        peer_free(peer);
+        user_entry_unref(user);
+        user_registry_free(registry);
+}
+
+int main(int argc, char **argv) {
+        test_setup();
+        return 0;
+}
