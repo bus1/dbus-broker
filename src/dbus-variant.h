7bc8af764abf912bc3e6c71317f182d1eaba6f81
David Herrmann <dh.herrmann@gmail.com>
2017-03-28 16:10:38 +0200

    variant: add skeleton builder/parser
    Add a skeleton builder/parser infrastructure for DBus variants.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/dbus-variant.h
+++ src/dbus-variant.h
@@ -7,6 +7,8 @@
 #include <c-macro.h>
 #include <stdlib.h>
 
+typedef struct DBusVariant DBusVariant;
+typedef struct DBusVariantLevel DBusVariantLevel;
 typedef struct DBusVariantType DBusVariantType;
 
 /*
@@ -31,6 +33,22 @@ typedef struct DBusVariantType DBusVariantType;
 #define DBUS_VARIANT_TYPE_o (0, 2, 'o', 1, 1)
 #define DBUS_VARIANT_TYPE_g (0, 0, 'g', 1, 1)
 #define DBUS_VARIANT_TYPE_v (0, 0, 'v', 1, 0)
+
+/*
+ * DBUS_VARIANT_SIZE_MAX_SHIFT: Maximum size of a DBus Variant, given as power
+ *                              of 2 (`shift value'). Note that this is taken
+ *                              from the maximum DBus message size, since DBus
+ *                              Variants are used in no other context.
+ *
+ * DBUS_VARIANT_SIZE_INITIAL_SHIFT: Initial size of the pre-allocated buffer
+ *                                  when writing DBus Variants, given as power
+ *                                  of 2.
+ *                                  XXX: This is set to page-size right now,
+ *                                       but should really be chosen by average
+ *                                       message sizes on a real system.
+ */
+#define DBUS_VARIANT_SIZE_MAX_SHIFT (27)
+#define DBUS_VARIANT_SIZE_INITIAL_SHIFT (12)
 
 /*
  * DBUS_VARIANT_TYPE_LENGTH_MAX: Maximum length of a DBus Type Signature, given
@@ -65,3 +83,51 @@ struct DBusVariantType {
 long dbus_variant_type_new_from_signature(DBusVariantType **infop,
                                           const char *signature,
                                           size_t n_signature);
+
+/**
+ * struct DBusVariantLevel - XXX
+ */
+struct DBusVariantLevel {
+        const DBusVariantType *root_type;
+        const DBusVariantType *i_type;
+        uint8_t n_type;
+        uint8_t container;
+        size_t i_buffer;
+        size_t n_buffer;
+};
+
+/**
+ * struct DBusVariant - XXX
+ */
+struct DBusVariant {
+        uint8_t *buffer;
+        size_t n_buffer;
+
+        int poison;
+        bool ro : 1;
+        bool big_endian : 1;
+
+        DBusVariantLevel *current;
+        DBusVariantLevel levels[DBUS_VARIANT_TYPE_DEPTH_MAX + 1];
+};
+
+#define DBUS_VARIANT_INIT(_var, _type) {                \
+                .current = (_var).levels,               \
+                .levels[0].root_type = (_type),         \
+                .levels[0].i_type = (_type),            \
+                .levels[0].n_type = (_type)->length,    \
+        }
+
+void dbus_variant_init(DBusVariant *var, const DBusVariantType *type);
+void dbus_variant_deinit(DBusVariant *var);
+
+void dbus_variant_parse(DBusVariant *var, bool big_endian, const void *data, size_t n_data);
+void dbus_variant_reset(DBusVariant *var);
+int dbus_variant_rewind(DBusVariant *var);
+int dbus_variant_steal(DBusVariant *var, void **datap, size_t *n_datap);
+
+bool dbus_variant_more(DBusVariant *var);
+void dbus_variant_vread(DBusVariant *var, const char *format, va_list args);
+void dbus_variant_vwrite(DBusVariant *var, const char *format, va_list args);
+
+C_DEFINE_CLEANUP(DBusVariant *, dbus_variant_deinit);
