82dcd1bc0aaff982c490b58dbdad46c104b15d42
David Herrmann <dh.herrmann@gmail.com>
2017-04-03 11:04:55 +0200

    driver: add initial fields parser
    Use c-dvar to parse the fields array of every message. Several
    semantics are still unclear and need to be checked with dbus-daemon(1).
    This is just the starting point for the parser.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/driver.c
+++ src/driver.c
@@ -2,6 +2,7 @@
  * DBus Driver
  */
 
+#include <c-dvar.h>
 #include <c-macro.h>
 #include <stdlib.h>
 #include "bus.h"
@@ -16,6 +17,16 @@
 #define DBUS_MESSAGE_TYPE_ERROR         (3)
 #define DBUS_MESSAGE_TYPE_SIGNAL        (4)
 
+#define DBUS_MESSAGE_FIELD_INVALID      (0)
+#define DBUS_MESSAGE_FIELD_PATH         (1)
+#define DBUS_MESSAGE_FIELD_INTERFACE    (2)
+#define DBUS_MESSAGE_FIELD_MEMBER       (3)
+#define DBUS_MESSAGE_FIELD_ERROR_NAME   (4)
+#define DBUS_MESSAGE_FIELD_REPLY_SERIAL (5)
+#define DBUS_MESSAGE_FIELD_DESTINATION  (6)
+#define DBUS_MESSAGE_FIELD_SENDER       (7)
+#define DBUS_MESSAGE_FIELD_SIGNATURE    (8)
+#define DBUS_MESSAGE_FIELD_UNIX_FDS     (9)
 
 typedef struct DriverMethod DriverMethod;
 typedef int (*DriverMethodFn) (Peer *peer, DBusMessage *message);
@@ -206,14 +217,83 @@ static int driver_handle_signal(Peer *peer,
 }
 
 int driver_handle_message(Peer *peer, DBusMessage *message) {
+        _c_cleanup_(c_dvar_type_freep) CDVarType *type = NULL;
+        _c_cleanup_(c_dvar_freep) CDVar *v = NULL;
         const char *path = NULL,
                    *interface = NULL,
                    *member = NULL,
                    *error_name = NULL,
                    *destination = NULL,
-                   /* *sender = NULL, XXX: verify? */
+                   *sender = NULL,
                    *signature = NULL;
         uint32_t reply_serial = 0, n_fds = 0;
+        uint8_t field;
+        int r;
+
+        /*
+         * XXX: Rather than allocating @type and @v, we should use their static
+         *      versions on the stack, once provided by c-dvar.
+         *      Also replace the NULL types in the dynamic readers below with
+         *      their pre-allocated respective types.
+         */
+
+        r = c_dvar_type_new_from_string(&type, "a(yv)");
+        if (r)
+                return (r > 0) ? -ENOTRECOVERABLE : r;
+
+        r = c_dvar_new(&v);
+        if (r)
+                return (r > 0) ? -ENOTRECOVERABLE : r;
+
+        c_dvar_begin_read(v, message->big_endian, type, message->fields, message->header->n_fields);
+
+        while (c_dvar_more(v)) {
+                /*
+                 * XXX: What should we do on duplicates?
+                 */
+
+                c_dvar_read(v, "[(y", &field);
+
+                switch (field) {
+                case DBUS_MESSAGE_FIELD_INVALID:
+                        return -EBADMSG;
+                case DBUS_MESSAGE_FIELD_PATH:
+                        c_dvar_read(v, "<o>)]", NULL, &path);
+                        break;
+                case DBUS_MESSAGE_FIELD_INTERFACE:
+                        c_dvar_read(v, "<o>)]", NULL, &path);
+                        break;
+                case DBUS_MESSAGE_FIELD_MEMBER:
+                        c_dvar_read(v, "<s>)]", NULL, &member);
+                        break;
+                case DBUS_MESSAGE_FIELD_ERROR_NAME:
+                        c_dvar_read(v, "<s>)]", NULL, &error_name);
+                        break;
+                case DBUS_MESSAGE_FIELD_REPLY_SERIAL:
+                        c_dvar_read(v, "<u>)]", NULL, &reply_serial);
+                        break;
+                case DBUS_MESSAGE_FIELD_DESTINATION:
+                        c_dvar_read(v, "<s>)]", NULL, &destination);
+                        break;
+                case DBUS_MESSAGE_FIELD_SENDER:
+                        /* XXX: check with dbus-daemon(1) on what to do */
+                        c_dvar_read(v, "<s>)]", NULL, &sender);
+                        break;
+                case DBUS_MESSAGE_FIELD_SIGNATURE:
+                        c_dvar_read(v, "<g>)]", NULL, &signature);
+                        break;
+                case DBUS_MESSAGE_FIELD_UNIX_FDS:
+                        c_dvar_read(v, "<u>)]", NULL, &n_fds);
+                        break;
+                default:
+                        c_dvar_skip(v, "v)]");
+                        break;
+                }
+        }
+
+        r = c_dvar_end_read(v);
+        if (r)
+                return (r > 0) ? -EBADMSG : r;
 
         if (_c_unlikely_(n_fds > message->n_fds))
                 return -EBADMSG;
