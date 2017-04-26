fcb981169b5ef9ed82877d654d1d7ea78a86c06a
Tom Gundersen <teg@jklm.no>
2017-04-26 14:45:34 +0200

    tests/broker: add some basic name handling tests
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/test-broker.c
+++ src/test-broker.c
@@ -10,6 +10,7 @@
 #include <sys/un.h>
 #include <systemd/sd-bus.h>
 #include <systemd/sd-id128.h>
+#include "dbus-protocol.h"
 #include "test.h"
 
 static sd_bus *connect_bus(struct sockaddr_un *address, socklen_t addrlen) {
@@ -110,7 +111,84 @@ static void test_setup(void) {
         assert(r == 0);
 }
 
+static void test_driver_names(sd_bus *bus1, sd_bus *bus2) {
+        sd_bus_message *message = NULL, *reply = NULL;
+        sd_bus_error error = SD_BUS_ERROR_NULL;
+        const char *unique_name1, *unique_name2, *reply_string;
+        uint32_t reply_u32;
+        int r;
+
+        r = sd_bus_get_unique_name(bus1, &unique_name1);
+        assert(r >= 0);
+
+        r = sd_bus_get_unique_name(bus2, &unique_name2);
+        assert(r >= 0);
+
+        /* grab a well-known name */
+        r = sd_bus_call_method(bus1, "org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus", "RequestName", &error, &reply,
+                               "su", "com.example.foobar", 0);
+        assert(r >= 0);
+        sd_bus_message_unref(message);
+
+        r = sd_bus_message_read(reply, "u", &reply_u32);
+        assert(r >= 0);
+        assert(reply_u32 == DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER);
+        sd_bus_message_unref(reply);
+
+        /* check that we now own it */
+        r = sd_bus_call_method(bus1, "org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus", "GetNameOwner", &error, &reply,
+                               "s", "com.example.foobar");
+        r = sd_bus_message_read(reply, "s", &reply_string);
+        assert(r >= 0);
+        assert(strcmp(reply_string, unique_name1) == 0);
+        sd_bus_message_unref(reply);
+
+        /* queue another owner request on the same name */
+        r = sd_bus_call_method(bus2, "org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus", "RequestName", &error, &reply,
+                               "su", "com.example.foobar", 0);
+        assert(r >= 0);
+        sd_bus_message_unref(message);
+
+        r = sd_bus_message_read(reply, "u", &reply_u32);
+        assert(r >= 0);
+        assert(reply_u32 == DBUS_REQUEST_NAME_REPLY_IN_QUEUE);
+        sd_bus_message_unref(reply);
+
+        /* explicitly release the name as the primary owner */
+        r = sd_bus_call_method(bus1, "org.freedesktop.DBus", "/org/freedesktop/DBus", "org.freedesktop.DBus", "ReleaseName", &error, &reply,
+                               "s", "com.example.foobar");
+        assert(r >= 0);
+        sd_bus_message_unref(message);
+
+        r = sd_bus_message_read(reply, "u", &reply_u32);
+        assert(r >= 0);
+        assert(reply_u32 == DBUS_RELEASE_NAME_REPLY_RELEASED);
+        sd_bus_message_unref(reply);
+}
+
+static void test_driver(void) {
+        _c_cleanup_(sd_bus_unrefp) sd_bus *bus1 = NULL, *bus2 = NULL;
+        struct sockaddr_un address;
+        socklen_t addrlen;
+        pthread_t thread;
+        int r;
+
+        thread = test_spawn_bus(&address, &addrlen);
+
+        bus1 = connect_bus(&address, addrlen);
+        bus2 = connect_bus(&address, addrlen);
+
+        test_driver_names(bus1, bus2);
+
+        r = pthread_kill(thread, SIGTERM);
+        assert(r == 0);
+
+        r = pthread_join(thread, NULL);
+        assert(r == 0);
+}
+
 int main(int argc, char **argv) {
         test_setup();
+        test_driver();
         return 0;
 }
