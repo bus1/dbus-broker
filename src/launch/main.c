1f0de23247202dea4fff1f6c898bd0dfca324e7b
David Herrmann <dh.herrmann@gmail.com>
2017-05-09 13:35:25 +0200

    launch: implement service activation
    Implement service activation. Parse service files from /usr and push
    them into the broker. Then make sure to forward activation-requests to
    pid1.
    
    Right now, we fall back to glib to do desktop-file parsing. Preferably,
    we wouldn't need it, but there is no proper alternative right now.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/launch/main.c
+++ src/launch/main.c
@@ -3,9 +3,11 @@
  */
 
 #include <c-macro.h>
+#include <c-rbtree.h>
 #include <c-string.h>
 #include <fcntl.h>
 #include <getopt.h>
+#include <glib.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <sys/prctl.h>
@@ -17,6 +19,7 @@
 #include "util/error.h"
 
 typedef struct Manager Manager;
+typedef struct Service Service;
 
 enum {
         _MAIN_SUCCESS,
@@ -24,19 +27,94 @@ enum {
         MAIN_FAILED,
 };
 
+struct Service {
+        Manager *manager;
+        CRBNode rb;
+        char *name;
+        char *exec;
+        char *unit;
+        char id[];
+};
+
 struct Manager {
         sd_event *event;
         sd_bus *bus;
         int fd_listen;
+        CRBTree services;
+        uint64_t service_ids;
 };
 
 static bool main_arg_verbose = false;
 static const char *main_arg_listen = "/var/run/dbus/system_bus_socket";
+static const char *main_arg_servicedir = "/usr/share/dbus-1/system-services";
 static bool main_arg_force = false;
 
+static int service_compare(CRBTree *t, void *k, CRBNode *n) {
+        Service *service = c_container_of(n, Service, rb);
+
+        return strcmp(k, service->id);
+}
+
+static Service *service_free(Service *service) {
+        if (!service)
+                return NULL;
+
+        c_rbtree_remove_init(&service->manager->services, &service->rb);
+        free(service->unit);
+        free(service->exec);
+        free(service->name);
+        free(service);
+
+        return NULL;
+}
+
+C_DEFINE_CLEANUP(Service *, service_free);
+
+static int service_new(Service **servicep, Manager *manager, const char *name, const char *exec, const char *unit) {
+        _c_cleanup_(service_freep) Service *service = NULL;
+        CRBNode **slot, *parent;
+
+        service = calloc(1, sizeof(*service) + C_DECIMAL_MAX(uint64_t) + 1);
+        if (!service)
+                return error_origin(-ENOMEM);
+
+        service->manager = manager;
+        service->rb = (CRBNode)C_RBNODE_INIT(service->rb);
+        sprintf(service->id, "%" PRIu64, ++manager->service_ids);
+
+        service->name = strdup(name);
+        if (!service->name)
+                return error_origin(-ENOMEM);
+
+        if (exec) {
+                service->exec = strdup(exec);
+                if (!service->exec)
+                        return error_origin(-ENOMEM);
+        }
+
+        if (unit) {
+                service->unit = strdup(unit);
+                if (!service->unit)
+                        return error_origin(-ENOMEM);
+        }
+
+        slot = c_rbtree_find_slot(&manager->services, service_compare, service->id, &parent);
+        assert(slot);
+        c_rbtree_add(&manager->services, parent, slot, &service->rb);
+
+        *servicep = service;
+        service = NULL;
+        return 0;
+}
+
 static Manager *manager_free(Manager *manager) {
+        Service *service;
+
         if (!manager)
                 return NULL;
+
+        while ((service = c_container_of(manager->services.root, Service, rb)))
+                service_free(service);
 
         c_close(manager->fd_listen);
         sd_bus_unref(manager->bus);
@@ -173,7 +251,46 @@ static int manager_fork(Manager *manager, int fd_controller) {
         return 0;
 }
 
-static int manager_on_name_activate(Manager *manager, sd_bus_message *m, const char *name) {
+static int manager_on_name_activate(Manager *manager, sd_bus_message *m, const char *id) {
+        _c_cleanup_(sd_bus_message_unrefp) sd_bus_message *signal = NULL;
+        Service *service;
+        int r;
+
+        service = c_rbtree_find_entry(&manager->services,
+                                      service_compare,
+                                      id,
+                                      Service,
+                                      rb);
+        if (!service) {
+                fprintf(stderr, "Activation request on unknown name '%s'\n", id);
+                return 0;
+        } else if (!strcmp(service->name, "org.freedesktop.systemd1")) {
+                /* pid1 activation requests are silently ignored */
+                return 0;
+        } else if (!service->unit) {
+                fprintf(stderr, "Missing systemd service to serve activation request on name '%s'\n", service->name);
+                return 0;
+        }
+
+        if (main_arg_verbose)
+                fprintf(stderr, "Activation request for '%s' -> '%s'\n", service->name, service->unit);
+
+        r = sd_bus_message_new_signal(manager->bus, &signal, "/org/freedesktop/DBus", "org.freedesktop.systemd1.Activator", "ActivationRequest");
+        if (r < 0)
+                return error_origin(r);
+
+        r = sd_bus_message_append(signal, "s", service->unit);
+        if (r < 0)
+                return error_origin(r);
+
+        r = sd_bus_message_set_destination(signal, "org.freedesktop.systemd1");
+        if (r < 0)
+                return error_origin(r);
+
+        r = sd_bus_send(manager->bus, signal, NULL);
+        if (r < 0)
+                return error_origin(r);
+
         return 0;
 }
 
@@ -193,6 +310,157 @@ static int manager_on_message(sd_bus_message *m, void *userdata, sd_bus_error *e
         }
 
         return r;
+}
+
+static int manager_load_service(Manager *manager, const char *path) {
+        gchar *name = NULL, *exec = NULL, *user = NULL, *unit = NULL;
+        _c_cleanup_(service_freep) Service *service = NULL;
+        _c_cleanup_(c_freep) char *object_path = NULL;
+        GKeyFile *f;
+        int r;
+
+        /*
+         * There seems to be no trivial way to properly parse D-Bus service
+         * files. Hence, we resort to glib GKeyFile to parse it as a Desktop
+         * File compatible ini-file.
+         *
+         * Preferably, we'd not have the glib dependency here, but it does not
+         * hurt much either. If anyone cares, feel free to provide `c-ini'.
+         */
+
+        if (main_arg_verbose)
+                fprintf(stderr, "Loading service '%s'\n", path);
+
+        f = g_key_file_new();
+
+        if (!g_key_file_load_from_file(f, path, G_KEY_FILE_NONE, NULL)) {
+                fprintf(stderr, "Cannot load service file '%s'\n", path);
+                r = 0;
+                goto exit;
+        }
+
+        name = g_key_file_get_string(f, "D-BUS Service", "Name", NULL);
+        exec = g_key_file_get_string(f, "D-BUS Service", "Exec", NULL);
+        user = g_key_file_get_string(f, "D-BUS Service", "User", NULL);
+        unit = g_key_file_get_string(f, "D-BUS Service", "SystemdService", NULL);
+
+        if (!name) {
+                fprintf(stderr, "Missing name in service file '%s'\n", path);
+                r = 0;
+                goto exit;
+        }
+
+        /*
+         * XXX: @user is unused so far, and we pass `0' as uid to dbus-broker.
+         *      Preferably, we would resolve @user to a uid, but we also do not
+         *      want to call into NSS..
+         *      For now, using 'root' seems good enough.
+         */
+
+        r = service_new(&service, manager, name, exec, unit);
+        if (r) {
+                r = error_trace(r);
+                goto exit;
+        }
+
+        r = asprintf(&object_path, "/org/bus1/DBus/Name/%s", service->id);
+        if (r < 0) {
+                r = error_origin(-ENOMEM);
+                goto exit;
+        }
+
+        r = sd_bus_call_method(manager->bus,
+                               NULL,
+                               "/org/bus1/DBus/Broker",
+                               "org.bus1.DBus.Broker",
+                               "AddName",
+                               NULL,
+                               NULL,
+                               "osu",
+                               object_path,
+                               service->name,
+                               0);
+        if (r < 0) {
+                r = error_origin(r);
+                goto exit;
+        }
+
+        service = NULL;
+        r = 0;
+
+exit:
+        g_free(unit);
+        g_free(user);
+        g_free(exec);
+        g_free(name);
+        g_key_file_free(f);
+        return r;
+}
+
+static int manager_load(Manager *manager) {
+        const char suffix[] = ".service";
+        _c_cleanup_(c_closedirp) DIR *dir;
+        struct dirent *de;
+        char *path;
+        size_t n;
+        int r;
+
+        dir = opendir(main_arg_servicedir);
+        if (!dir) {
+                if (errno == ENOENT || errno == ENOTDIR)
+                        return 0;
+                else
+                        return error_origin(-errno);
+        }
+
+        for (errno = 0, de = readdir(dir);
+             de;
+             errno = 0, de = readdir(dir)) {
+                if (de->d_name[0] == '.')
+                        continue;
+
+                n = strlen(de->d_name);
+                if (n <= strlen(suffix))
+                        continue;
+                if (strcmp(de->d_name + n - strlen(suffix), suffix))
+                        continue;
+
+                r = asprintf(&path, "%s/%s", main_arg_servicedir, de->d_name);
+                if (r < 0)
+                        return error_origin(-ENOMEM);
+
+                r = manager_load_service(manager, path);
+                free(path);
+                if (r)
+                        return error_trace(r);
+        }
+        if (errno > 0)
+                return error_origin(-errno);
+
+        return 0;
+}
+
+static int manager_setup(Manager *manager) {
+        int r;
+
+        r = manager_load(manager);
+        if (r)
+                return error_trace(r);
+
+        r = sd_bus_call_method(manager->bus,
+                               NULL,
+                               "/org/bus1/DBus/Broker",
+                               "org.bus1.DBus.Broker",
+                               "AddListener",
+                               NULL,
+                               NULL,
+                               "oh",
+                               "/org/bus1/DBus/Listener/0",
+                               manager->fd_listen);
+        if (r < 0)
+                return error_origin(r);
+
+        return 0;
 }
 
 static int manager_run(Manager *manager) {
@@ -227,18 +495,9 @@ static int manager_run(Manager *manager) {
         if (r < 0)
                 return error_origin(r);
 
-        r = sd_bus_call_method(manager->bus,
-                               NULL,
-                               "/org/bus1/DBus/Broker",
-                               "org.bus1.DBus.Broker",
-                               "AddListener",
-                               NULL,
-                               NULL,
-                               "oh",
-                               "/org/bus1/DBus/Listener/0",
-                               manager->fd_listen);
+        r = manager_setup(manager);
         if (r < 0)
-                return error_origin(r);
+                return error_fold(r);
 
         r = sd_event_loop(manager->event);
         if (r < 0)
