bus1/dbus-broker

commit 8458f773331c5cff6ec9f9dadfa6338e75d6810d

match: rework eavesdrop rules
Now each registry contains two sets of rules: eavesdrop rules and
regular matching rules.

The only distinction between the two is that eavesdrop rules apply
to all messages, whereas regular match rules only apply to
broadcasts. The distinction is made in the iterator, and not in the
match logic itself.

This means that unicast messages are only affected by the installed
eavesdrop rules, and never by any unrelated regular match rules.

Signed-off-by: Tom Gundersen <teg@jklm.no>

src/match.c
@@ -74,9 +74,6 @@ static bool match_rule_keys_match_filter(MatchRuleKeys *keys, MatchFilter *filte
         if (keys->filter.type && keys->filter.type != filter->type)
                 return false;
 
-        if (!keys->eavesdrop && filter->destination != UNIQUE_NAME_ID_INVALID)
-                return false;
-
         if (keys->filter.destination != UNIQUE_NAME_ID_INVALID && keys->filter.destination != filter->destination)
                 return false;
 
@@ -400,8 +397,9 @@ void match_rule_link(MatchRule *rule, MatchRegistry *registry) {
 
         rule->registry = registry;
         if (rule->keys.eavesdrop)
-                ++registry->n_eavesdrop;
-        c_list_link_tail(&registry->rule_list, &rule->registry_link);
+                c_list_link_tail(&registry->eavesdrop_list, &rule->registry_link);
+        else
+                c_list_link_tail(&registry->rule_list, &rule->registry_link);
 }
 
 void match_rule_unlink(MatchRule *rule) {
@@ -409,8 +407,6 @@ void match_rule_unlink(MatchRule *rule) {
                 return;
 
         c_list_unlink_init(&rule->registry_link);
-        if (rule->keys.eavesdrop)
-                --rule->registry->n_eavesdrop;
         rule->registry = NULL;
 }
 
@@ -432,27 +428,36 @@ int match_rule_get(MatchRule **rulep, MatchOwner *owner, const char *rule_string
         return 0;
 }
 
-MatchRule *match_rule_next(MatchRegistry *registry, MatchRule *rule, MatchFilter *filter) {
-        CList *link;
-
-        if (filter->destination != UNIQUE_NAME_ID_INVALID &&
-            !registry->n_eavesdrop)
-                /* only eavesdroppers can receive broadcasts with a destination */
-                return NULL;
-
-        if (!rule)
-                link = c_list_loop_first(&registry->rule_list);
-        else
-                link = c_list_loop_next(&rule->registry_link);
-
-        while (link != &registry->rule_list) {
-                rule = c_list_entry(link, MatchRule, registry_link);
-
-                if (match_rule_keys_match_filter(&rule->keys, filter))
+static MatchRule *match_rule_next(MatchRegistry *registry, MatchRule *rule, bool unicast) {
+        if (!rule) {
+                rule = c_list_first_entry(&registry->eavesdrop_list, MatchRule, registry_link);
+                if (rule)
                         return rule;
 
-                link = c_list_loop_next(link);
+                if (unicast)
+                        return NULL;
+
+                return c_list_first_entry(&registry->rule_list, MatchRule, registry_link);
+        } else if (rule->keys.eavesdrop) {
+                if (rule != c_list_last_entry(&registry->eavesdrop_list, MatchRule, registry_link))
+                        return c_list_entry(rule->registry_link.next, MatchRule, registry_link);
+                if (unicast)
+                        return NULL;
+                return c_list_first_entry(&registry->rule_list, MatchRule, registry_link);
+        } else {
+                if (rule != c_list_last_entry(&registry->rule_list, MatchRule, registry_link))
+                        return c_list_entry(&registry->rule_list.next, MatchRule, registry_link);
         }
+
+        return NULL;
+}
+
+MatchRule *match_rule_next_match(MatchRegistry *registry, MatchRule *rule, MatchFilter *filter) {
+        bool unicast = filter->destination != UNIQUE_NAME_ID_INVALID;
+
+        for (rule = match_rule_next(registry, rule, unicast); rule; rule = match_rule_next(registry, rule, unicast))
+                if (match_rule_keys_match_filter(&rule->keys, filter))
+                        return rule;
 
         return NULL;
 }
@@ -463,7 +468,7 @@ void match_registry_init(MatchRegistry *registry) {
 
 void match_registry_deinit(MatchRegistry *registry) {
         assert(c_list_is_empty(&registry->rule_list));
-        assert(!registry->n_eavesdrop);
+        assert(c_list_is_empty(&registry->eavesdrop_list));
 }
 
 void match_owner_init(MatchOwner *owner) {

src/test-match.c
@@ -142,7 +142,7 @@ static bool test_match(const char *match_string, MatchFilter *filter) {
 
         match_rule_link(rule, &registry);
 
-        rule1 = match_rule_next(&registry, NULL, filter);
+        rule1 = match_rule_next_match(&registry, NULL, filter);
         assert(!rule1 || rule1 == rule);
 
         match_rule_free(rule);

src/bus.c
@@ -66,7 +66,7 @@ static int bus_broadcast_to_matches(MatchRegistry *matches, MatchFilter *filter,
         MatchRule *rule;
         int r;
 
-        for (rule = match_rule_next(matches, NULL, filter); rule; rule = match_rule_next(matches, rule, filter)) {
+        for (rule = match_rule_next_match(matches, NULL, filter); rule; rule = match_rule_next_match(matches, rule, filter)) {
                 Peer *peer = c_container_of(rule->owner, Peer, owned_matches);
 
                 /* exclude the destination from broadcasts */

src/match.h
@@ -57,7 +57,7 @@ struct MatchRule {
 
 struct MatchRegistry {
         CList rule_list;
-        size_t n_eavesdrop;
+        CList eavesdrop_list;
 };
 
 struct MatchOwner {
@@ -73,11 +73,11 @@ void match_rule_link(MatchRule *rule, MatchRegistry *registry);
 void match_rule_unlink(MatchRule *rule);
 int match_rule_get(MatchRule **rulep, MatchOwner *owner, const char *rule_string);
 
-MatchRule *match_rule_next(MatchRegistry *registry, MatchRule *rule, MatchFilter *filter);
+MatchRule *match_rule_next_match(MatchRegistry *registry, MatchRule *rule, MatchFilter *filter);
 
-#define MATCH_REGISTRY_INIT(_x) {                                       \
-                .rule_list = (CList)C_LIST_INIT((_x).rule_list),        \
-                .n_eavesdrop = 0,                                       \
+#define MATCH_REGISTRY_INIT(_x) {                                               \
+                .rule_list = (CList)C_LIST_INIT((_x).rule_list),                \
+                .eavesdrop_list = (CList)C_LIST_INIT((_x).eavesdrop_list),      \
         }
 
 void match_registry_init(MatchRegistry *registry);

commit f63e90351353c57529d40e6ed7764de90307d24d

match: fix match iterator
This was a bug introduced in 8458f773331c5cff6ec9f9dadfa6338e75d6810d. Add
a test case to catch similar bugs in the future.

Signed-off-by: Tom Gundersen <teg@jklm.no>

src/match.c
@@ -445,12 +445,14 @@ static MatchRule *match_rule_next(MatchRegistry *registry, MatchRule *rule, bool
         } else if (rule->keys.eavesdrop) {
                 if (rule != c_list_last_entry(&registry->eavesdrop_list, MatchRule, registry_link))
                         return c_list_entry(rule->registry_link.next, MatchRule, registry_link);
+
                 if (unicast)
                         return NULL;
+
                 return c_list_first_entry(&registry->rule_list, MatchRule, registry_link);
         } else {
                 if (rule != c_list_last_entry(&registry->rule_list, MatchRule, registry_link))
-                        return c_list_entry(&registry->rule_list.next, MatchRule, registry_link);
+                        return c_list_entry(rule->registry_link.next, MatchRule, registry_link);
         }
 
         return NULL;

src/test-match.c
@@ -258,6 +258,63 @@ static void test_individual_matches(void) {
         assert(!test_match("arg0namespace=com.example", &filter));
 }
 
+void test_iterator(void) {
+        MatchRegistry registry;
+        MatchOwner owner1, owner2;
+        MatchFilter filter;
+        MatchRule *rule, *rule1, *rule2, *rule3, *rule4;
+        int r;
+
+        match_registry_init(&registry);
+        match_owner_init(&owner1);
+        match_owner_init(&owner2);
+        match_filter_init(&filter);
+
+        r = match_rule_new(&rule1, &owner1, "eavesdrop=true");
+        assert(!r);
+
+        match_rule_link(rule1, &registry);
+
+        r = match_rule_new(&rule2, &owner1, "");
+        assert(!r);
+
+        match_rule_link(rule2, &registry);
+
+        r = match_rule_new(&rule3, &owner2, "eavesdrop=true");
+        assert(!r);
+
+        match_rule_link(rule3, &registry);
+
+        r = match_rule_new(&rule4, &owner2, "");
+        assert(!r);
+
+        match_rule_link(rule4, &registry);
+
+        rule = match_rule_next_match(&registry, NULL, &filter);
+        assert(rule == rule1);
+
+        rule = match_rule_next_match(&registry, rule, &filter);
+        assert(rule == rule3);
+
+        rule = match_rule_next_match(&registry, rule, &filter);
+        assert(rule == rule2);
+
+        rule = match_rule_next_match(&registry, rule, &filter);
+        assert(rule == rule4);
+
+        rule = match_rule_next_match(&registry, rule, &filter);
+        assert(!rule);
+
+        match_rule_free(rule4);
+        match_rule_free(rule3);
+        match_rule_free(rule2);
+        match_rule_free(rule1);
+        match_owner_deinit(&owner2);
+        match_owner_deinit(&owner1);
+        match_registry_deinit(&registry);
+
+}
+
 int main(int argc, char **argv) {
         MatchOwner owner = {};
 
@@ -268,6 +325,8 @@ int main(int argc, char **argv) {
         test_validate_keys(&owner);
 
         test_individual_matches();
+
+        test_iterator();
 
         match_owner_deinit(&owner);
         return 0;