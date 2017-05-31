ad91d88c9d3305590a860e089004f6b70d3f9342
David Herrmann <dh.herrmann@gmail.com>
2017-05-31 10:00:16 +0200

    test: add unit tests for util/error.h
    Add some basic tests for util/error.h, testing for symbol visibility
    and basic behavior.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

---
+++ src/util/test-error.c
@@ -0,0 +1,52 @@
+/*
+ * Test Error Handling
+ */
+
+#include <c-macro.h>
+#include <stdlib.h>
+#include "error.h"
+
+static void test_basic(void) {
+        int r;
+
+        /* error_origin(0) is a no-op */
+        r = error_origin(0);
+        assert(!r);
+
+        /* make sure no double evaluation happens */
+        r = error_origin(--r);
+        assert(r == -1);
+
+        /* error_origin() always returns <=0 */
+        r = error_origin(1);
+        assert(r < 0);
+
+        /* error_trace(0) is a no-op */
+        r = error_trace(0);
+        assert(!r);
+
+        /* make sure no double evaluation happens */
+        r = error_trace(--r);
+        assert(r == -1);
+
+        /* error_trace() never modifies the error code */
+        r = error_trace(1);
+        assert(r == 1);
+
+        /* error_fold(0) is a no-op */
+        r = error_fold(0);
+        assert(!r);
+
+        /* make sure no double evaluation happens */
+        r = error_fold(--r);
+        assert(r == -1);
+
+        /* error_fold() never returns positive codes */
+        r = error_fold(1);
+        assert(r < 0);
+}
+
+int main(int argc, char **argv) {
+        test_basic();
+        return 0;
+}
