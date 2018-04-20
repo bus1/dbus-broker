3f4ffb1e37922bd6b84320382ef743770f28f634
David Herrmann <dh.herrmann@gmail.com>
2018-04-20 16:12:37 +0200

    util/sockopt: fix forward-declaration of Log
    We need a proper typedef-forward-declaration, rather than just the
    struct. Fix it.
    
    Signed-off-by: David Herrmann <dh.herrmann@gmail.com>

--- src/util/sockopt.h
+++ src/util/sockopt.h
@@ -7,7 +7,7 @@
 #include <c-macro.h>
 #include <stdlib.h>
 
-struct Log;
+typedef struct Log Log;
 
 int sockopt_get_peersec(int fd, char **labelp, size_t *lenp);
 int sockopt_get_peergroups(int fd, Log *log, uid_t uid, gid_t gid, gid_t **gidsp, size_t *n_gidsp);
