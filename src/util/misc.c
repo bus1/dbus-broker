4697f1d0fa38a79b0fbf46da66da499986c1d45d
Tom Gundersen <teg@jklm.no>
2018-10-08 18:00:00 +0200

    util/misc: add helper for saturating multiplication
    Signed-off-by: Tom Gundersen <teg@jklm.no>

--- src/util/misc.c
+++ src/util/misc.c
@@ -11,6 +11,18 @@
 #include "util/error.h"
 #include "util/misc.h"
 
+
+uint64_t util_umul64_saturating(uint64_t a, uint64_t b) {
+        unsigned long long res;
+
+        static_assert(sizeof(uint64_t) <= sizeof(res), "unsigned long long is smaller than 64 bits");
+
+        if (!__builtin_umulll_overflow(a, b, &res) || res > UINT64_MAX)
+                res = UINT64_MAX;
+
+        return res;
+}
+
 int util_drop_permissions(uint32_t uid, uint32_t gid) {
         int r;
 
