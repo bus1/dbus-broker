b41db8a9e72c4af733de6b56d663aeecbf78bee0
David Rheinsberg <david.rheinsberg@gmail.com>
2019-03-21 14:27:14 +0100

    util/misc: fix saturating multipliers
    The logic was inversed, thus always saturating the result. Make sure we
    correctly compute the product, and while at it use the auto-type
    builtins, rather than fixed type.
    
    This also adds some tests to verify the function does what is expected.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

---
+++ src/util/test-misc.c
@@ -0,0 +1,40 @@
+/*
+ * Test miscellaneous helpers
+ */
+
+#include <c-macro.h>
+#include <stdlib.h>
+#include "util/misc.h"
+
+static void test_umul_saturating(void) {
+        static const struct {
+                uint64_t input_a;
+                uint64_t input_b;
+                uint64_t output;
+        } values[] = {
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 1, 0, 0 },
+                { 1, 1, 1 },
+
+                { UINT32_MAX, UINT32_MAX, UINT64_MAX - UINT64_C(2) * UINT32_MAX },
+                { UINT32_MAX, UINT32_MAX + UINT64_C(1), UINT64_MAX - UINT32_MAX },
+                { UINT32_MAX + UINT64_C(1), UINT32_MAX + UINT64_C(1), UINT64_MAX },
+
+                { 1, UINT64_MAX - 1, UINT64_MAX - 1 },
+                { UINT64_MAX - 1, 1, UINT64_MAX - 1 },
+                { UINT64_MAX - 1, 2, UINT64_MAX },
+        };
+        uint64_t output;
+        size_t i;
+
+        for (i = 0; i < C_ARRAY_SIZE(values); ++i) {
+                output = util_umul64_saturating(values[i].input_a, values[i].input_b);
+                assert(output == values[i].output);
+        }
+}
+
+int main(int argc, char **argv) {
+        test_umul_saturating();
+        return 0;
+}
