c945e554f2f6a5b736c3fe887a0adbfbe84579dd
David Rheinsberg <david.rheinsberg@gmail.com>
2020-03-02 10:14:55 +0100

    util/sockopt: 'gid' -> 'primary_gid'
    Lets make clear what this parameter exactly means. It is the primary
    group of the user.
    
    Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

--- src/util/sockopt.h
+++ src/util/sockopt.h
@@ -10,4 +10,4 @@
 typedef struct Log Log;
 
 int sockopt_get_peersec(int fd, char **labelp, size_t *lenp);
-int sockopt_get_peergroups(int fd, Log *log, uid_t uid, gid_t gid, gid_t **gidsp, size_t *n_gidsp);
+int sockopt_get_peergroups(int fd, Log *log, uid_t uid, gid_t primary_gid, gid_t **gidsp, size_t *n_gidsp);
