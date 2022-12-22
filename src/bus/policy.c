bus1/dbus-broker

commit 8b1e969c1eff0bd2756b406b3d96d53bc6ddba48

util/apparmor: add apparmor support
Add full AppArmor support using libapparmor. This requires af_unix
mediation support in the kernel (which has not yet landed upstream).

Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>
Signed-off-by: David Rheinsberg <david.rheinsberg@gmail.com>

src/bus/policy.c
@@ -10,6 +10,7 @@
 #include "bus/name.h"
 #include "bus/policy.h"
 #include "dbus/protocol.h"
+#include "util/apparmor.h"
 #include "util/common.h"
 #include "util/error.h"
 #include "util/selinux.h"
@@ -413,6 +414,10 @@ int policy_registry_new(PolicyRegistry **registryp, const char *fallback_seclabe
 
         *registry = (PolicyRegistry)POLICY_REGISTRY_NULL;
 
+        r = bus_apparmor_registry_new(&registry->apparmor, fallback_seclabel);
+        if (r)
+                return error_fold(r);
+
         r = bus_selinux_registry_new(&registry->selinux, fallback_seclabel);
         if (r)
                 return error_fold(r);
@@ -444,6 +449,7 @@ PolicyRegistry *policy_registry_free(PolicyRegistry *registry) {
 
         policy_batch_unref(registry->default_batch);
         bus_selinux_registry_unref(registry->selinux);
+        bus_apparmor_registry_unref(registry->apparmor);
         free(registry);
 
         return NULL;
@@ -691,9 +697,11 @@ int policy_registry_import(PolicyRegistry *registry, CDVar *v) {
 
         c_dvar_read(v, "]bs)>", &apparmor, &bustype);
 
-        if (apparmor)
-                /* XXX: AppArmor is currently not supported. */
-                return POLICY_E_INVALID;
+        if (apparmor) {
+                r = bus_apparmor_set_bus_type(registry->apparmor, bustype ? bustype : "");
+                if (r)
+                        return error_trace(r);
+        }
 
         r = c_dvar_get_poison(v);
         if (r)
@@ -730,6 +738,7 @@ int policy_snapshot_new(PolicySnapshot **snapshotp,
 
         *snapshot = (PolicySnapshot)POLICY_SNAPSHOT_NULL;
 
+        snapshot->apparmor = bus_apparmor_registry_ref(registry->apparmor);
         snapshot->selinux = bus_selinux_registry_ref(registry->selinux);
 
         snapshot->seclabel = strdup(seclabel);
@@ -778,6 +787,7 @@ PolicySnapshot *policy_snapshot_free(PolicySnapshot *snapshot) {
                 policy_batch_unref(snapshot->batches[snapshot->n_batches]);
         free(snapshot->seclabel);
         bus_selinux_registry_unref(snapshot->selinux);
+        bus_apparmor_registry_unref(snapshot->apparmor);
         free(snapshot);
 
         return NULL;
@@ -796,6 +806,7 @@ int policy_snapshot_dup(PolicySnapshot *snapshot, PolicySnapshot **newp) {
 
         *new = (PolicySnapshot)POLICY_SNAPSHOT_NULL;
 
+        new->apparmor = bus_apparmor_registry_ref(snapshot->apparmor);
         new->selinux = bus_selinux_registry_ref(snapshot->selinux);
 
         new->seclabel = strdup(snapshot->seclabel);
@@ -834,6 +845,14 @@ int policy_snapshot_check_own(PolicySnapshot *snapshot, const char *name_str) {
         CRBNode *rb;
         size_t i;
         int v, r;
+
+        r = bus_apparmor_check_own(snapshot->apparmor, snapshot->seclabel, name_str);
+        if (r) {
+                if (r == BUS_APPARMOR_E_DENIED)
+                        return POLICY_E_APPARMOR_ACCESS_DENIED;
+
+                return error_fold(r);
+        }
 
         r = bus_selinux_check_own(snapshot->selinux, snapshot->seclabel, name_str);
         if (r) {
@@ -1047,6 +1066,15 @@ int policy_snapshot_check_send(PolicySnapshot *snapshot,
         size_t i;
         int r;
 
+        r = bus_apparmor_check_xmit(snapshot->apparmor, true, snapshot->seclabel, subject_seclabel,
+                                    subject, subject_id, path, interface, method);
+        if (r) {
+                if (r == BUS_APPARMOR_E_DENIED)
+                        return POLICY_E_APPARMOR_ACCESS_DENIED;
+
+                return error_fold(r);
+        }
+
         r = bus_selinux_check_send(snapshot->selinux, snapshot->seclabel, subject_seclabel);
         if (r) {
                 if (r == SELINUX_E_DENIED)
@@ -1085,6 +1113,16 @@ int policy_snapshot_check_receive(PolicySnapshot *snapshot,
                                   size_t n_fds) {
         PolicyVerdict verdict = POLICY_VERDICT_INIT;
         size_t i;
+        int r;
+
+        r = bus_apparmor_check_xmit(snapshot->apparmor, false, subject_seclabel, snapshot->seclabel,
+                                    subject, subject_id, path, interface, method);
+        if (r) {
+                if (r == BUS_APPARMOR_E_DENIED)
+                        return POLICY_E_APPARMOR_ACCESS_DENIED;
+
+                return error_fold(r);
+        }
 
         for (i = 0; i < snapshot->n_batches; ++i)
                 policy_snapshot_check_xmit(snapshot->batches[i],

src/util/apparmor-fallback.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "util/apparmor.h"
+#include "util/error.h"
 
 /**
  * bus_apparmor_is_enabled() - checks if AppArmor is currently enabled
@@ -37,5 +38,38 @@ int bus_apparmor_is_enabled(bool *enabledp) {
  */
 int bus_apparmor_dbus_supported(bool *supportedp) {
         *supportedp = false;
+        return 0;
+}
+
+int bus_apparmor_registry_new(struct BusAppArmorRegistry **registryp, const char *fallback_context) {
+        *registryp = NULL;
+        return 0;
+}
+
+BusAppArmorRegistry *bus_apparmor_registry_ref(BusAppArmorRegistry *registry) {
+        return NULL;
+}
+
+BusAppArmorRegistry *bus_apparmor_registry_unref(BusAppArmorRegistry *registry) {
+        return NULL;
+}
+
+int bus_apparmor_set_bus_type(BusAppArmorRegistry *registry, const char *bustype) {
+        return 0;
+}
+
+int bus_apparmor_check_own(struct BusAppArmorRegistry *registry, const char *owner_context,
+                           const char *name) {
+        return 0;
+}
+
+int bus_apparmor_check_xmit(BusAppArmorRegistry *registry, bool check_send,
+                            const char *sender_context, const char *receiver_context,
+                            NameSet *subject, uint64_t subject_id,
+                            const char *path, const char *interface, const char *method) {
+        return 0;
+}
+
+int bus_apparmor_check_eavesdrop(BusAppArmorRegistry *registry, const char *context) {
         return 0;
 }

commit 3e4e4316095a7fadcaf5468884f739257f7cd6e3

util/apparmor: only check sending messages
Checking if the destination is allowed to receive during
send time is not the same as checking if a message is allowed
to be received at receive time.

Fix this by following the dbus-daemon implementation more
closely and handle everything in bus_apparmor_check_send().

Reported-by: David Rheinsberg <david.rheinsberg@gmail.com>
Fixes: 8b1e969c1eff ("util/apparmor: add apparmor support")
Signed-off-by: Sebastian Reichel <sebastian.reichel@collabora.com>

src/bus/policy.c
@@ -1064,7 +1064,7 @@ int policy_snapshot_check_send(PolicySnapshot *snapshot,
         size_t i;
         int r;
 
-        r = bus_apparmor_check_xmit(snapshot->apparmor, true, snapshot->seclabel, subject_seclabel,
+        r = bus_apparmor_check_send(snapshot->apparmor, snapshot->seclabel, subject_seclabel,
                                     subject, subject_id, path, interface, method);
         if (r) {
                 if (r == BUS_APPARMOR_E_DENIED)
@@ -1111,16 +1111,6 @@ int policy_snapshot_check_receive(PolicySnapshot *snapshot,
                                   size_t n_fds) {
         PolicyVerdict verdict = POLICY_VERDICT_INIT;
         size_t i;
-        int r;
-
-        r = bus_apparmor_check_xmit(snapshot->apparmor, false, subject_seclabel, snapshot->seclabel,
-                                    subject, subject_id, path, interface, method);
-        if (r) {
-                if (r == BUS_APPARMOR_E_DENIED)
-                        return POLICY_E_APPARMOR_ACCESS_DENIED;
-
-                return error_fold(r);
-        }
 
         for (i = 0; i < snapshot->n_batches; ++i)
                 policy_snapshot_check_xmit(snapshot->batches[i],

src/util/apparmor-fallback.c
@@ -62,7 +62,7 @@ int bus_apparmor_check_own(struct BusAppArmorRegistry *registry, const char *own
         return 0;
 }
 
-int bus_apparmor_check_xmit(BusAppArmorRegistry *registry, bool check_send,
+int bus_apparmor_check_send(BusAppArmorRegistry *registry,
                             const char *sender_context, const char *receiver_context,
                             NameSet *subject, uint64_t subject_id,
                             const char *path, const char *interface, const char *method) {

src/util/apparmor.c
@@ -485,9 +485,8 @@ int bus_apparmor_check_own(struct BusAppArmorRegistry *registry,
 }
 
 /**
- * bus_apparmor_check_xmit() - check if the given transaction is allowed
+ * bus_apparmor_check_send() - check if the given transaction is allowed
  * @registry:           AppArmor registry to operate on
- * @check_send:         true if sending should be checked, false if receiving
  * @sender_context:     security context of the sender
  * @receiver_context:   security context of the receiver, or NULL
  * @subject:            List of names
@@ -506,8 +505,7 @@ int bus_apparmor_check_own(struct BusAppArmorRegistry *registry,
  * Return: 0 if the transaction is allowed, BUS_APPARMOR_E_DENIED if it is not,
  *         or a negative error code on failure.
  */
-int bus_apparmor_check_xmit(BusAppArmorRegistry *registry,
-                            bool check_send,
+int bus_apparmor_check_send(BusAppArmorRegistry *registry,
                             const char *sender_context,
                             const char *receiver_context,
                             NameSet *subject,
@@ -519,7 +517,7 @@ int bus_apparmor_check_xmit(BusAppArmorRegistry *registry,
         _c_cleanup_(c_freep) char *receiver_context_dup = NULL;
         char *sender_security_label, *sender_security_mode;
         char *receiver_security_label, *receiver_security_mode;
-        int r, allow, audit;
+        int r, src_allow = false, src_audit = true, dst_allow = false, dst_audit = true;
 
         if (!registry->bustype)
                 return 0;
@@ -534,46 +532,69 @@ int bus_apparmor_check_xmit(BusAppArmorRegistry *registry,
         sender_security_label = aa_splitcon(sender_context_dup, &sender_security_mode);
         receiver_security_label = aa_splitcon(receiver_context_dup, &receiver_security_mode);
 
-        if (is_unconfined(sender_security_label, sender_security_mode))
-                return 0;
-        if (is_unconfined(receiver_security_label, receiver_security_mode))
-                return 0;
-
-        if (check_send)
+        if (is_unconfined(sender_security_label, sender_security_mode)) {
+                src_allow = true;
+                src_audit = false;
+        } else {
                 r = apparmor_message_query(true,
                                            sender_security_label,
                                            registry->bustype,
                                            receiver_security_label,
                                            subject, subject_id, path,
-                                           interface, method, &allow, &audit);
-        else
+                                           interface, method, &src_allow, &src_audit);
+
+                if (r)
+                        return error_fold(r);
+        }
+
+        if (is_unconfined(receiver_security_label, receiver_security_mode)) {
+                dst_allow = true;
+                dst_audit = false;
+        } else {
                 r = apparmor_message_query(false,
                                            receiver_security_label,
                                            registry->bustype,
                                            sender_security_label,
                                            subject, subject_id, path,
-                                           interface, method, &allow, &audit);
-        if (r)
-                return error_fold(r);
+                                           interface, method, &dst_allow, &dst_audit);
+                if (r)
+                        return error_fold(r);
+        }
 
-        if (audit)
-                bus_apparmor_log(
-                        registry,
-                        "apparmor=\"%s\" operation=\"dbus_%s\" bus=\"%s\" "
-                        "path=\"%s\" interface=\"%s\" method=\"%s\"",
-                        allow ? "ALLOWED" : "DENIED",
-                        check_send ? "send" : "receive",
+        if (string_equal(sender_security_mode, "complain"))
+                src_allow = 1;
+        if (string_equal(receiver_security_mode, "complain"))
+                dst_allow = 1;
+
+        if (src_audit) {
+                bus_apparmor_log(registry,
+                        "apparmor=\"%s\" bus=\"%s\" path=\"%s\" interface=\"%s\" method=\"%s\" "
+                        "label=\"%s\" peer_label=\"%s\"",
+                        src_allow ? "ALLOWED" : "DENIED",
                         registry->bustype,
                         path,
                         interface,
-                        method
+                        method,
+                        sender_security_label,
+                        receiver_security_label
                 );
+        }
 
-        if ((check_send && string_equal(sender_security_mode, "complain")) ||
-            (!check_send && string_equal(receiver_security_mode, "complain")))
-                allow = 1;
+        if (dst_audit) {
+                bus_apparmor_log(registry,
+                        "apparmor=\"%s\" bus=\"%s\" path=\"%s\" interface=\"%s\" method=\"%s\" "
+                        "label=\"%s\" peer_label=\"%s\"",
+                        dst_allow ? "ALLOWED" : "DENIED",
+                        registry->bustype,
+                        path,
+                        interface,
+                        method,
+                        receiver_security_label,
+                        sender_security_label
+                );
+        }
 
-        return allow ? 0 : BUS_APPARMOR_E_DENIED;
+        return (src_allow && dst_allow) ? 0 : BUS_APPARMOR_E_DENIED;
 }
 
 /**

src/util/apparmor.h
@@ -29,7 +29,7 @@ int bus_apparmor_set_bus_type(BusAppArmorRegistry *registry, const char *bustype
 
 int bus_apparmor_check_own(struct BusAppArmorRegistry *registry, const char *context,
                            const char *name);
-int bus_apparmor_check_xmit(BusAppArmorRegistry *registry, bool check_send,
+int bus_apparmor_check_send(BusAppArmorRegistry *registry,
                             const char *sender_context, const char *receiver_context,
                             NameSet *subject, uint64_t subject_id,
                             const char *path, const char *interface, const char *method);