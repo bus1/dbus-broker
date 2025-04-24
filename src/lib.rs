a7bf1439ba5fd96fdcf2355ae402b2299dca077b
David Rheinsberg <david@readahead.eu>
2025-04-24 10:54:44 +0200

    build: add an internal Rust library
    Extend the build system to compile Rust sources in our tree into an
    internal, static library. This is then linked into `libbus.a`, which
    itself is our static library that contains everything but the
    entrypoints.
    
    This does not add any Rust code, but merely extends the build-system to
    support adding Rust code.
    
    Additionally, we add infrastructure to call `rust-bindgen` on our own C
    headers, so we get clean access to it from Rust.
    
    Note that this significantly bumps the build-time requirements of the
    project:
    
    - Meson 1.7.0 (January 2025): Needed for support of Rust Edition 2024
    - Rust 1.85 (February 2025): Needed for support of Rust Edition 2024
    - rust-bindgen 0.71 (December 2024): Needed for `--rust-edition` and
      `--rust-target` configuration.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+//! # Rust Bus Library
+//!
+//! This library combines all Rust utilities of dbus-broker. It is linked to
+//! most executables of dbus-broker as utility library, relying on link-time
+//! garbage collection to drop any unneeded code.
+
+/// # Generated Code
+///
+/// This module exposes all bindgen-generated (or otherwise generated) code,
+/// and makes it available to the entire crate.
+///
+/// Note that any C/Rust interaction is done via bindgen-generated C
+/// definitions, to guarantee that the Rust and C definitions never get out
+/// of sync.
+pub(crate) mod generated {
+}
+
+#[cfg(test)]
+mod test {
+    // Simple dummy to show the test-suite in the test results, even if
+    // other tests are conditionally disabled.
+    #[test]
+    fn availability() {
+    }
+}
