4882a0c80ba1f1861d8e8f547a3309ac27964602
David Rheinsberg <david@readahead.eu>
2025-04-30 15:19:41 +0200

    util/acct: implement a new hierarchical accounting system
    This is a replacement for `util/user.[ch]`. It is written in Rust, but
    exposes a C API that can be used by dbus-broker.
    
    The new accounting system uses the `acct_*` namespace to ease the
    transition and avoid naming conflicts. Furthermore, the system is
    written in Rust and relies on the new build-system support for Rust
    code.
    
    The accounting system works with the same algorithm as the previous
    flat-accounting model, but extends it to a 2-layered hierarchy. The
    algorithm is adopted to support an arbitrarily nested hierarchy, but the
    code will only expose 2 layers:
    
    1) As before, all users are assigned a flat resource limit that they get
       full control over. But when user boundaries are crossed, a quota is
       applied to guarantee fair resource sharing. The algorithm used is
       still the `fairdist`, but the allocators were improved on.
    
    2) Beneath the user quotas, a new layer is introduced. Rather than users
       claiming resources, now all operations originate from an `Actor`.
       These always operate on behalf of a user, but allow us to distinguish
       different actors of the same user. A new resource tracing system is
       now added analog to the user quotas. It operates above the quotas and
       ensures we can semi-protect individual actors of the same user (which
       was not possible before). We do not provide a fully fair policy on
       this level, however, since no privilege boundaries are crossed.
       Instead, this provides a semi-fair policy which is a lot more
       lenient, but still allows us to protect against failing clients.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/clib.rs
+++ src/clib.rs
@@ -1,1 +1,188 @@
 //! # C-Rust Bridge
+//!
+//! This module implements a C API for the rust library bundles with the
+//! bus broker.
+
+extern crate alloc;
+extern crate core;
+
+// Helper macro to ensure two expressions have the same type.
+//
+// This macro relies on if/else expressions to require both blocks
+// to have the same type. Otherwise, this will yield to compile errors.
+macro_rules! type_eq {
+    ($a:expr, $b:expr) => {
+        const _: () = {
+            if true {
+                ($a)
+            } else {
+                ($b)
+            };
+        };
+    };
+}
+
+mod acct {
+    use alloc::rc;
+    use core::{ffi, mem, ptr};
+    use rbus::generated::util_acct;
+    use rbus::util::acct;
+
+    #[unsafe(export_name = "acct_new")]
+    pub unsafe extern "C" fn acct_new(
+        acctp: *mut *mut util_acct::Acct,
+        maxima_c: *const [util_acct::acct_value_t; acct::N_SLOTS],
+    ) -> ffi::c_int {
+        type_eq!(acct_new, util_acct::acct_new);
+
+        let maxima = unsafe { &*maxima_c };
+        let acct = acct::Acct::new(maxima);
+
+        unsafe { ptr::write(acctp, rc::Rc::into_raw(acct) as _) };
+        0
+    }
+
+    #[unsafe(export_name = "acct_free")]
+    pub unsafe extern "C" fn acct_free(
+        acct_c: *mut util_acct::Acct,
+    ) -> *mut util_acct::Acct {
+        type_eq!(acct_free, util_acct::acct_free);
+
+        let acct: rc::Rc<acct::Acct> = unsafe { rc::Rc::from_raw(acct_c as _) };
+        drop(acct);
+
+        ptr::null_mut()
+    }
+
+    #[unsafe(export_name = "acct_ref_user")]
+    pub unsafe extern "C" fn acct_ref_user(
+        acct_c: *mut util_acct::Acct,
+        userp: *mut *mut util_acct::AcctUser,
+        id_c: util_acct::acct_id_t,
+    ) -> ffi::c_int {
+        type_eq!(acct_ref_user, util_acct::acct_ref_user);
+
+        let acct: rc::Rc<acct::Acct> = unsafe { rc::Rc::from_raw(acct_c as _) };
+        let user = acct.get_user(id_c);
+
+        unsafe { ptr::write(userp, rc::Rc::into_raw(user) as _) };
+        mem::forget(acct);
+        0
+    }
+
+    #[unsafe(export_name = "acct_user_ref")]
+    pub unsafe extern "C" fn acct_user_ref(
+        user_c: *mut util_acct::AcctUser,
+    ) -> *mut util_acct::AcctUser {
+        type_eq!(acct_user_ref, util_acct::acct_user_ref);
+
+        let user: rc::Rc<acct::User> = unsafe { rc::Rc::from_raw(user_c as _) };
+        mem::forget(user.clone());
+
+        rc::Rc::into_raw(user) as _
+    }
+
+    #[unsafe(export_name = "acct_user_unref")]
+    pub unsafe extern "C" fn acct_user_unref(
+        user_c: *mut util_acct::AcctUser,
+    ) -> *mut util_acct::AcctUser {
+        type_eq!(acct_user_unref, util_acct::acct_user_unref);
+
+        let user: rc::Rc<acct::User> = unsafe { rc::Rc::from_raw(user_c as _) };
+        drop(user);
+
+        ptr::null_mut()
+    }
+
+    #[unsafe(export_name = "acct_user_new_actor")]
+    pub unsafe extern "C" fn acct_user_new_actor(
+        user_c: *mut util_acct::AcctUser,
+        actorp: *mut *mut util_acct::AcctActor,
+    ) -> ffi::c_int {
+        type_eq!(acct_user_new_actor, util_acct::acct_user_new_actor);
+
+        let user: rc::Rc<acct::User> = unsafe { rc::Rc::from_raw(user_c as _) };
+        let actor = acct::Actor::with(&user);
+
+        unsafe { ptr::write(actorp, rc::Rc::into_raw(actor) as _) };
+        mem::forget(user);
+        0
+    }
+
+    #[unsafe(export_name = "acct_user_charge")]
+    pub unsafe extern "C" fn acct_user_charge(
+        user_c: *mut util_acct::AcctUser,
+        charge_raw_c: *mut util_acct::AcctCharge,
+        claimant_c: *mut util_acct::AcctActor,
+        amount_c: *const [util_acct::acct_value_t; acct::N_SLOTS],
+    ) -> ffi::c_int {
+        type_eq!(acct_user_charge, util_acct::acct_user_charge);
+
+        let user: rc::Rc<acct::User> = unsafe { rc::Rc::from_raw(user_c as _) };
+        let claimant: rc::Rc<acct::Actor> = unsafe { rc::Rc::from_raw(claimant_c as _) };
+        let amount = unsafe { &*amount_c };
+
+        let Some(charge) = user.charge(&claimant, amount) else {
+            return util_acct::ACCT_E_QUOTA;
+        };
+
+        let charge_raw = unsafe { &mut *charge_raw_c };
+        if !charge_raw.trace.is_null() {
+            let charge_prev = unsafe { acct::Charge::from_raw(*charge_raw) };
+            charge_prev.discharge();
+        }
+
+        ptr::write(charge_raw_c, charge.into_raw());
+
+        mem::forget(claimant);
+        mem::forget(user);
+        0
+    }
+
+    #[unsafe(export_name = "acct_actor_ref")]
+    pub unsafe extern "C" fn acct_actor_ref(
+        actor_c: *mut util_acct::AcctActor,
+    ) -> *mut util_acct::AcctActor {
+        type_eq!(acct_actor_ref, util_acct::acct_actor_ref);
+
+        let actor: rc::Rc<acct::Actor> = unsafe { rc::Rc::from_raw(actor_c as _) };
+        mem::forget(actor.clone());
+
+        rc::Rc::into_raw(actor) as _
+    }
+
+    #[unsafe(export_name = "acct_actor_unref")]
+    pub unsafe extern "C" fn acct_actor_unref(
+        actor_c: *mut util_acct::AcctActor,
+    ) -> *mut util_acct::AcctActor {
+        type_eq!(acct_actor_unref, util_acct::acct_actor_unref);
+
+        let actor: rc::Rc<acct::Actor> = unsafe { rc::Rc::from_raw(actor_c as _) };
+        drop(actor);
+
+        ptr::null_mut()
+    }
+
+    #[unsafe(export_name = "acct_actor_charge")]
+    pub unsafe extern "C" fn acct_actor_charge(
+        actor_c: *mut util_acct::AcctActor,
+        charge: *mut util_acct::AcctCharge,
+        claimant: *mut util_acct::AcctActor,
+        amount: *const [util_acct::acct_value_t; acct::N_SLOTS],
+    ) -> ffi::c_int {
+        type_eq!(acct_actor_charge, util_acct::acct_actor_charge);
+
+        let actor: rc::Rc<acct::Actor> = unsafe { rc::Rc::from_raw(actor_c as _) };
+        let user_raw = rc::Rc::as_ptr(actor.user());
+
+        let r = acct_user_charge(
+            user_raw as _,
+            charge,
+            claimant,
+            amount,
+        );
+
+        mem::forget(actor);
+        r
+    }
+}
--- src/rlib.rs
+++ src/rlib.rs
@@ -4,6 +4,10 @@
 //! most executables of dbus-broker as utility library, relying on link-time
 //! garbage collection to drop any unneeded code.
 
+#![allow(
+    clippy::redundant_field_names,
+)]
+
 #![no_std]
 
 extern crate alloc;
@@ -24,6 +28,14 @@ extern crate core;
     non_upper_case_globals,
 )]
 pub mod generated {
+    pub mod util_acct;
+}
+
+/// # Utilities
+///
+/// A collection of independent utilities for the bus broker.
+pub mod util {
+    pub mod acct;
 }
 
 #[cfg(test)]
