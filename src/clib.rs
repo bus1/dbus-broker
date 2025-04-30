0bc75cced5f5f8dd78abb6f60e8fbf81d041d1b9
David Rheinsberg <david@readahead.eu>
2025-04-30 15:19:41 +0200

    util/acct: implement a new hierarchical accounting system
    This is a replacement for `util/user.[ch]`. It is written in Rust, but
    exposes a C API that can be used by dbus-broker.
    
    The new accounting system uses the `acct_*` namespace to ease the
    transition and avoid naming conflicts. Furthermore, the system is
    written in Rust and relies on the new build-system support for Rust
    code.
    
    The accounting system works with the same algorithm as the previous
    flat-accounting model, but extends it to a 2-layered hierarchy. The
    algorithm is adopted to support an arbitrarily nested hierarchy, but the
    code will only expose 2 layers:
    
    1) As before, all users are assigned a flat resource limit that they get
       full control over. But when user boundaries are crossed, a quota is
       applied to guarantee fair resource sharing. The algorithm used is
       still the `fairdist`, but the allocators were improved on.
    
    2) Beneath the user quotas, a new layer is introduced. Rather than users
       claiming resources, now all operations originate from an `Actor`.
       These always operate on behalf of a user, but allow us to distinguish
       different actors of the same user. A new resource tracing system is
       now added analog to the user quotas. It operates above the quotas and
       ensures we can semi-protect individual actors of the same user (which
       was not possible before). We do not provide a fully fair policy on
       this level, however, since no privilege boundaries are crossed.
       Instead, this provides a semi-fair policy which is a lot more
       lenient, but still allows us to protect against failing clients.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/clib.rs
+++ src/clib.rs
@@ -1,1 +1,39 @@
 //! # C-Rust Bridge
+//!
+//! XXX
+
+mod acct {
+    use rbus::generated::util_acct;
+    use rbus::util::acct;
+
+    #[unsafe(export_name = "acct_new")]
+    pub extern "C" fn acct_new(
+        acctp: *mut *mut util_acct::Acct,
+        maxima: *const [util_acct::acct_value_t; acct::N_SLOTS],
+    ) -> ::core::ffi::c_int {
+        unsafe {
+            let acct = acct::Acct::new(&*maxima);
+            *acctp = std::rc::Rc::into_raw(acct) as *mut _;
+        }
+        0
+    }
+
+    #[unsafe(export_name = "acct_free")]
+    pub extern "C" fn acct_free(
+        acct_c: *mut util_acct::Acct,
+    ) -> *mut util_acct::Acct {
+        let _ = unsafe { std::rc::Rc::from_raw(acct_c) };
+        core::ptr::null_mut()
+    }
+
+    /*
+    #[unsafe(export_name = "acct_user_ref")]
+    pub extern "C" fn user_ref(
+        user: *mut util_acct::AcctUser,
+    ) -> *mut util_acct::AcctUser {
+        let u = unsafe { rc::Rc::from_raw(user as *mut User) };
+        core::mem::forget(u.clone());
+        user
+    }
+    */
+}
