2a1e4912eed4a40eebb034c7b451b818ffb93b66
David Rheinsberg <david@readahead.eu>
2025-10-15 13:57:21 +0200

    bus/metrics: dump metrics on any request
    Start tracking metrics clients and dump the metrics on any request. So
    far we use an empty metrics set, but this introduces the infrastructure
    to dump any kind of metrics snapshot.
    
    Signed-off-by: David Rheinsberg <david@readahead.eu>

--- src/bus/metrics.c
+++ src/bus/metrics.c
@@ -2,6 +2,7 @@
  * Metrics Listener
  */
 
+#include <c-list.h>
 #include <c-stdaux.h>
 #include <stdlib.h>
 #include <sys/epoll.h>
@@ -11,10 +12,127 @@
 #include "util/dispatch.h"
 #include "util/error.h"
 
+static MetricsClient *metrics_client_free(MetricsClient *client) {
+        if (!client)
+                return NULL;
+
+        free(client->buffer);
+        dispatch_file_deinit(&client->socket_file);
+        c_close(client->socket_fd);
+        c_list_unlink(&client->metrics_link);
+        free(client);
+
+        return NULL;
+}
+
+C_DEFINE_CLEANUP(MetricsClient *, metrics_client_free);
+
+static int metrics_client_dispatch(DispatchFile *file) {
+        MetricsClient *client = c_container_of(file, MetricsClient, socket_file);
+        _c_cleanup_(c_closep) int fd = -1;
+        uint32_t events = dispatch_file_events(file);
+        bool hup = false;
+
+        if (events & EPOLLHUP) {
+                hup = true;
+        } else if (events & EPOLLOUT) {
+                ssize_t len;
+
+                len = send(
+                        client->socket_fd,
+                        client->buffer + client->i_buffer,
+                        client->n_buffer - client->i_buffer,
+                        MSG_DONTWAIT | MSG_NOSIGNAL
+                );
+                if (len < 0) {
+                        switch (errno) {
+                        case EAGAIN:
+                                dispatch_file_clear(file, EPOLLOUT);
+                                break;
+                        case ECOMM:
+                        case ECONNABORTED:
+                        case ECONNRESET:
+                        case EHOSTDOWN:
+                        case EHOSTUNREACH:
+                        case EIO:
+                        case ENOBUFS:
+                        case ENOMEM:
+                        case EPIPE:
+                        case EPROTO:
+                        case EREMOTEIO:
+                        case ESHUTDOWN:
+                        case ETIMEDOUT:
+                                hup = true;
+                                break;
+                        default:
+                                return error_origin(-errno);
+                        }
+                } else if (len >= 0) {
+                        client->i_buffer += len;
+                        if (client->i_buffer >= client->n_buffer)
+                                hup = true;
+                        else
+                                dispatch_file_clear(file, EPOLLOUT);
+                }
+        }
+
+
+        if (hup)
+                metrics_client_free(client);
+
+        return 0;
+}
+
+static int metrics_client_new_with_fd(MetricsClient **clientp, Metrics *metrics, int fd) {
+        _c_cleanup_(metrics_client_freep) MetricsClient *client = NULL;
+        int r;
+
+        client = calloc(1, sizeof(*client));
+        if (!client)
+                return error_origin(-ENOMEM);
+
+        c_list_link_tail(&metrics->client_list, &client->metrics_link);
+        client->socket_fd = -1;
+
+        r = dispatch_file_init(
+                &client->socket_file,
+                metrics->socket_file.context,
+                metrics_client_dispatch,
+                fd,
+                EPOLLHUP | EPOLLOUT,
+                EPOLLOUT
+        );
+        if (r)
+                return error_fold(r);
+
+        client->socket_fd = fd;
+        dispatch_file_select(&metrics->socket_file, EPOLLHUP | EPOLLOUT);
+
+        *clientp = client;
+        client = NULL;
+        return 0;
+}
+
+static int metrics_compile(MetricsClient *client) {
+        int r;
+
+        c_assert(!client->buffer);
+
+        r = asprintf(
+                (void *)&client->buffer,
+                "# EOF"
+        );
+        if (r < 0)
+                return error_origin(-errno);
+
+        return 0;
+}
+
 static int metrics_dispatch(DispatchFile *file) {
         Metrics *metrics = c_container_of(file, Metrics, socket_file);
-        _c_cleanup_(peer_freep) Peer *peer = NULL;
         _c_cleanup_(c_closep) int fd = -1;
+        MetricsClient *client;
+        int r;
 
         if (!(dispatch_file_events(file) & EPOLLIN))
                 return 0;
@@ -39,7 +157,14 @@ static int metrics_dispatch(DispatchFile *file) {
                 }
         }
 
-        /* FIXME: Dump metrics on the client. */
+        r = metrics_client_new_with_fd(&client, metrics, fd);
+        if (r)
+                return error_fold(r);
+        fd = -1; /* consumed by client */
+
+        r = metrics_compile(client);
+        if (r)
+                return error_fold(r);
 
         return 0;
 }
@@ -77,6 +202,11 @@ int metrics_init_with_fd(Metrics *m,
  * metrics_deinit() - XXX
  */
 void metrics_deinit(Metrics *metrics) {
+        MetricsClient *client;
+
+        while ((client = c_list_first_entry(&metrics->client_list, MetricsClient, metrics_link)))
+                metrics_client_free(client);
+
         dispatch_file_deinit(&metrics->socket_file);
         metrics->socket_fd = c_close(metrics->socket_fd);
         metrics->bus = NULL;
